0bc2bbe42f4ff3a4dc64bb7d8bd2bbb8ffa39f90
Martin Duhem <martin@c-webhosting.org>
2015-04-14 22:52:29 +0200

    Don't use `@quasiquote` annotation for token quasiquote

--- quasiquotes/src/main/scala/scala/meta/tokenquasiquotes.scala
+++ quasiquotes/src/main/scala/scala/meta/tokenquasiquotes.scala
@@ -1,11 +1,15 @@
 package scala.meta
 
-import scala.meta.parsermacro.quasiquote
+import scala.meta.parsermacro.TokenReificationMacros
 
 object tokenquasiquotes {
 
-  implicit def parseTokens(implicit dialect: Dialect): Parse[Vector[Token]] = Parse.apply { _.tokens }
-
-  @quasiquote[Vector[Token]]('toks) implicit class XtensionQuasiquoteTokens(ctx: StringContext)
+  implicit class XtensionQuasiquoteTokens(val ctx: StringContext) {
+    object toks {
+      import scala.language.experimental.macros
+      def apply(args: Any*)(implicit dialect: Dialect): Vector[Token] = macro TokenReificationMacros.apply
+      def unapply(scrutinee: Any)(implicit dialect: Dialect): Any = macro TokenReificationMacros.unapply
+    }
+  }
 
 }
